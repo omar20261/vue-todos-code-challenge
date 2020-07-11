efaa51d843cb6c8784cded581374733c2ee5cb9f
mero mero <mero20261@gmail.com>
2020-07-11 11:48:11 +0200

    added #num in todo-item Html + Css

--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <v-card>
     <v-card-title primary-title>
+      <span class="todo-num">{{ '#' + (index + 1) }}</span>
       <div>
         <h3 class="headline mb-0">
           {{ item.title }}
@@ -40,4 +41,9 @@ export default {
 </script>
 
 <style lang="css">
+  .todo-num{
+    font-size: 24px;
+    margin: 10px;
+    float: right;
+  }
 </style>
